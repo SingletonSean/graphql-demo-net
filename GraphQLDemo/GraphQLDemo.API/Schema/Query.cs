22888bbefdf15f160a0f3381772cd40ec8864f41
SingletonSean <singletonsean4@gmail.com>
2021-07-18 16:45:18 -0400

    Setup server project

---
+++ GraphQLDemo/GraphQLDemo.API/Schema/Query.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLDemo.API.Schema
+{
+    public class Query
+    {
+        public string Instructions => "Smash that like button and subscribe to SingletonSean";
+    }
+}
